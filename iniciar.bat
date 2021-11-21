1129df7d1c0ae458db56311fe72a98ec7e0cf671
Skull Rain <94234562+SkullRa1n@users.noreply.github.com>
2021-11-21 18:47:01 +0000

    Delete iniciar.bat

--- iniciar.bat
+++
@@ -1,7 +0,0 @@
-@echo off 
-:: Criado por M0rthifxr (https://github.com/M0rthifxr) [ 14 / 11 / 2021 ] 
-:: -*Leia o readme para saber sobre direitos e permissões!*- 
-title M0rthifxr Selfbot 
-:START 
-node hiro.js 
-goto START 
